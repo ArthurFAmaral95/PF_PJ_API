1457630256ff042a785d964de8dc709657be3537
Arthur Amaral <arthuramaral6@gmail.com>
2025-09-04 05:49:15 -0300

    pessoa_juridica_creator_composer

--- src/main/routes/pessoa_juridica_routes.py
+++ src/main/routes/pessoa_juridica_routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint, jsonify, request
 from src.views.http_types.http_request import HttpRequest
 from src.main.composer.pessoa_juridica_lister_all_composer import pessoa_juridica_lister_all_composer
 from src.main.composer.pessoa_juridica_lister_composer import pessoa_juridica_lister_composer
+from src.main.composer.pessoa_juridica_creator_composer import pessoa_juridica_creator_composer
 
 pessoa_juridica_route_bp = Blueprint('pessoa_juridica_routes', __name__)
 
@@ -21,3 +22,12 @@ def list_pessoa_juridica(client_id):
 
   http_response = view.handle(http_request=http_request)
   return jsonify(http_response.body), http_response.status_code
+
+@pessoa_juridica_route_bp.route('/pj', methods=['POST'])
+def create_pessoa_juridica():
+  http_request = HttpRequest(body=request.json)
+  view = pessoa_juridica_creator_composer()
+
+  http_response = view.handle(http_request=http_request)
+  return jsonify(http_response.body), http_response.status_code
+  
